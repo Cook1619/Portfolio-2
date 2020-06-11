c915ad6dccd859906edfa5af262e872214380b29
Cook1619 <cook6348@gmail.com>
2020-06-11 16:32:58 -0500

    Navbar Complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles/navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav class="navbar">
+        <h1>Mathew Cook</h1>
+    <ul class="nav-links">
+      <li class="nav-item"><a href="#">About</a></li>
+      <li class="nav-item"><a href="#">Skills</a></li>
+      <li class="nav-item"><a href="#">Blog</a></li>
+      <li class="nav-item"><a href="#">Github</a></li>
+    </ul>
+  </nav>
+  );
+};
+
+export default Navbar;
