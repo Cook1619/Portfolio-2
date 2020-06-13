ef2c6adcd2765c39963e52078c0b3a8c12529e1b
Cook1619 <cook6348@gmail.com>
2020-06-13 13:12:32 -0500

    Re-worked nav bar

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './styles/main.css'
+import React from "react";
+import "./styles/main.css";
 
 const Main = () => {
-    return (
-        <div>
-            <h1>TESTING</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="main-container">
+        <p>Hello, I'm Mathew Cook</p>
+      </div>
+      <div className="second-container">
+        <h2>I'm a FullStack Web Developer</h2>
+      </div>
+    </div>
+  );
+};
 
 export default Main;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,15 +3,26 @@ import "./styles/navbar.css";
 
 const Navbar = () => {
   return (
-    <nav class="navbar">
+    <div>
+      <nav class="navbar">
         <h1 className="name">Mathew Cook</h1>
-    <ul class="nav-links">
-      <li class="nav-item"><a href="#">About</a></li>
-      <li class="nav-item"><a href="#">Skills</a></li>
-      <li class="nav-item"><a href="#">Blog</a></li>
-      <li class="nav-item"><a href="#">Github</a></li>
-    </ul>
-  </nav>
+        <ul class="nav-links">
+          <li class="nav-item">
+            <a href="#">About</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Skills</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Blog</a>
+          </li>
+          <li class="nav-item">
+            <a href="#">Github</a>
+          </li>
+        </ul>
+      </nav>
+      <hr className="nav-bottom"/>
+    </div>
   );
 };
 
