db0f663dcadec8395bb61e53b9f894419aa19c73
Cook1619 <cook6348@gmail.com>
2020-06-13 14:09:46 -0500

    Adding more fun animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { motion } from "framer-motion";
 import "./styles/navbar.css";
 
 const Navbar = () => {
   return (
     <div>
       <nav class="navbar">
-        <h1>Mathew Cook</h1>
+        <motion.div
+          whileHover={{ scale: 1.2, rotate: 360 }}>
+          <h1>Mathew Cook</h1>
+        </motion.div>
+
         <ul class="nav-links">
           <li class="nav-item">
             <a href="#">About</a>
