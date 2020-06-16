b35b1edbdcb3e71119dac64919702baf448a4456
Cook1619 <cook6348@gmail.com>
2020-06-16 15:15:00 -0500

    Working on layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Main from './components/Main';
+import Projects from './components/Projects.jsx'
 
 function App() {
   return (
     <div>
       <Navbar />
       <Main />
+      <Projects />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./styles/main.css";
 import { motion } from "framer-motion";
+import img from './img/github_pic.jpeg'
 
 const Main = () => {
   return (
-    <div>
+    <div className="container">
       <motion.div
-        initial={{ opacity: 0}}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 1, ease: "easeIn" }}
         className="main-container"
@@ -14,23 +15,25 @@ const Main = () => {
         <p>Hello, I'm Mathew Cook</p>
       </motion.div>
       <motion.div
-        initial={{ opacity: 0}}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 2, ease: "easeIn"  }}
+        transition={{ duration: 2, ease: "easeIn" }}
         className="second-container"
       >
         <h2>I'm a FullStack Web Developer</h2>
       </motion.div>
       <motion.div
-        initial={{ opacity: 0}}
+        initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
-        transition={{ duration: 3, ease: "easeIn"  }}
+        transition={{ duration: 3, ease: "easeIn" }}
         className="third-container"
       >
         <p>
           I love creating web applications to solve problems and just for fun
         </p>
       </motion.div>
+      <img src={img} alt="Matt Cook"/>
+      <hr className="main-bottom" />
     </div>
   );
 };
