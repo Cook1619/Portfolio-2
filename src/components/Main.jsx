09308d8824c5facea9821845dd3aadaea27afa14
Cook1619 <cook6348@gmail.com>
2020-06-21 09:10:28 -0500

    main component styled with tailwind

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,44 +5,42 @@ import img from "./img/github_pic.jpeg";
 
 const Main = () => {
   return (
-    <div className="container">
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1, ease: "easeIn" }}
-        className="main-heading"
-      >
-        <p>Hello, I'm Mathew Cook</p>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2, ease: "easeIn" }}
-        className="main-title"
-      >
-        <h2>I'm a FullStack Web Developer</h2>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3, ease: "easeIn" }}
-        className="main-title-description"
-      >
-        <p>
-          I love creating web applications to solve problems and just for fun
-        </p>
-      </motion.div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 3, ease: "easeIn" }}
-        className="main-img"
-      >
-        <img src={img} alt="Matt Cook" />
-      </motion.div>
-      <div className="main-footer">
-        <hr className="main-hr" />
+    <div>
+      <div className="grid grid-rows-4 grid-flow-col gap-1 max-w-2xl m-auto pt-20">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 1, ease: "easeIn" }}
+          className="row-span-1 col-span-2 ..."
+        >
+          Hello, I'm Mathew Cook
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 2, ease: "easeIn" }}
+          className="row-span-1 col-span-2 ..."
+        >
+          I'm a FullStack Web Developer
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 3, ease: "easeIn" }}
+          className="row-span-1 col-span-2 ..."
+        >
+          I like presenting users to relavant data to their position
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 4, ease: "easeIn" }}
+          className="row-span-4 ..."
+        >
+          <img className="w-48 h-48" src={img} alt="Matt Cook" />
+        </motion.div>
       </div>
+      <hr className="projects-hr" />
     </div>
   );
 };
